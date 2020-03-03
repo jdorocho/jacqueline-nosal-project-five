0d90bedfa76197187f44a424cbd028ee4e604829
Jacqui Nosal <jacqueline.dchow@gmail.com>
2020-03-03 10:31:33 -0500

    Updates

---
+++ precache-manifest.978937cca387455220d733ffeeb57b8e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0289ece475b42839be175e2a82da0a4",
+    "url": "/jacqueline-nosal-project-five/index.html"
+  },
+  {
+    "revision": "8cf1e1ac4bc2d6f72f58",
+    "url": "/jacqueline-nosal-project-five/static/css/main.c417d857.chunk.css"
+  },
+  {
+    "revision": "c9de1f6fd125e1ab0419",
+    "url": "/jacqueline-nosal-project-five/static/js/2.a38c1a6a.chunk.js"
+  },
+  {
+    "revision": "07a707eba59e3e3ed782f77b31f34780",
+    "url": "/jacqueline-nosal-project-five/static/js/2.a38c1a6a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8cf1e1ac4bc2d6f72f58",
+    "url": "/jacqueline-nosal-project-five/static/js/main.15f364ec.chunk.js"
+  },
+  {
+    "revision": "f3e7337cb9b454cc27d7",
+    "url": "/jacqueline-nosal-project-five/static/js/runtime-main.621f7c3a.js"
+  }
+]);
